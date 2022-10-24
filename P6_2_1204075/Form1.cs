acada11ce39540514b8aa8d3557cc4cdf62b9cbb
christyuda <foong.wob.5@gmail.com>
2022-10-24 14:39:17 +0700

    Add project files.

---
+++ P6_2_1204075/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P6_2_1204075
+{
+    public partial class Form1 : Form
+    {
+        private string filePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult userResponse = openFileDialog1.ShowDialog();
+                if (userResponse == DialogResult.OK) 
+                {
+                    
+                    filePath = openFileDialog1.FileName.ToString();
+                    MessageBox.Show("You succesfully opened: " + filePath + "'", "Succes",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                }
+                else
+                {
+                    MessageBox.Show("You Canceled the open file operaton.", "Warning",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+    }
+}
